13ad6cf07fc031a409db58ba43f87f3f1a592c6b
amantajatii <diazamantajatisusilo@mail.ugm.ac.id>
2025-05-25 19:49:38 +0700

    section 1 done

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -25,7 +25,7 @@ contract SimpleStorage {
     function addPerson (string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
+    } 
 
 }
 
